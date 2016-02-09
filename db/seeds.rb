e6c162e07632544e1e8cebb17179838be8246bd9
navase <margot.sauter@gmail.com>
2016-02-09 22:53:37 +0100

    Models, database and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Song.destroy_all
+Artist.destroy_all
+
+naragonia = Artist.create({
+  name: "Naragonia",
+  biography: "Naragonia brengt eigenzinnige folk met een verfijnde, stevige energie. Het duo ontstond spontaan in de zomer van 2003 op het festival Andancas in Portugal.",
+  genre: "Folk"
+})
+
+kings_of_convenience = Artist.create({
+  name: "Kings of Convenience",
+  biography: "Kings of Convenience is an indie folk-pop duo from Bergen, Norway. The musical group is known for their delicate tunes, calming voices, and intricate and subtle guitar melodies.",
+  genre: "Pop"
+})
+
+alio = Song.create({ title: "Alio", artist_id: naragonia.id })
+carabel = Song.create({ title: "Carabel", artist_id: naragonia.id })
+dance_with_you = Song.create({ title: "I'd rather dance with you", artist_id: kings_of_convenience.id })
+cayman_islands = Song.create({ title: "Cayman Islands", artist_id: kings_of_convenience.id})
