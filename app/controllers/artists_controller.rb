81925d622a73aad5acd2c83d3f35d748e0a7db4d
navase <margot.sauter@gmail.com>
2016-02-11 18:45:18 +0100

    Artists new and edit and navigation

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,4 +6,44 @@ class ArtistsController < ApplicationController
   def show
     @artist = Artist.find(params[:id])
   end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    if @artist = Artist.create(artist_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    if @artist.update(artist_params)
+      redirect_to @artist
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @artist = Artist.find (params[:id])
+    if @artist.destroy
+      redirect_to root_path
+    else
+      redirect_to @artist
+    end
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :biography, :genre)
+  end
 end
