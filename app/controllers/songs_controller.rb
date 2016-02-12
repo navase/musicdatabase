18d5b0f2d28d4fc13282057648ab2e77b779ee9b
navase <margot.sauter@gmail.com>
2016-02-12 11:11:50 +0100

    Songs new and edit - not working yet

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,4 +6,44 @@ class SongsController < ApplicationController
   def show
     @song = Song.find(params[:id])
   end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    if @song = Song.create(song_params)
+      redirect_to songs_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to @song
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    if @song.destroy
+      redirect_to songs_path
+    else
+      redirect_to @song
+    end
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :artist_id)
+  end
 end
